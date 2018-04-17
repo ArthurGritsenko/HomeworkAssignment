4e7aa50e89149f9d21498b904271f5cef5595cf0
Arthur Gritsenko <gricenko.a.v@gmail.com>
2018-04-17 16:12:44 -0500

    Cretead basic project structure; Added Test Sample Data;

---
+++ HomeworkAssignment.Core/Configurations/ContainerConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace HomeworkAssignment.Core.Configurations
+{
+    public static class ContainerConfig
+    {
+        public static void Configure()
+        {
+            var unityContainer = new UnityContainer();
+        }
+    }
+}
--- HomeworkAssignment/Program.cs
+++ HomeworkAssignment/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HomeworkAssignment.Core.Configurations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace HomeworkAssignment
     {
         static void Main(string[] args)
         {
+            ContainerConfig.Configure();
         }
     }
 }
